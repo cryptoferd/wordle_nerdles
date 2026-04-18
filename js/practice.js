7c2030b391661ab6744bd363d8d7b345f67c757f
cryptoferd <102158235+cryptoferd@users.noreply.github.com>
2026-04-18 08:50:40 -0500

    Change 'ENTER' key to '↳' in keyboard layout

--- js/practice.js
+++ js/practice.js
@@ -8,7 +8,7 @@ const WORD_LENGTH = 5;
 const KEYBOARD_ROWS = [
   ['Q','W','E','R','T','Y','U','I','O','P'],
   ['A','S','D','F','G','H','J','K','L'],
-  ['ENTER','Z','X','C','V','B','N','M','⌫'],
+  ['↳','Z','X','C','V','B','N','M','⌫'],
 ];
 
 const state = {
